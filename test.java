460552f8cb29bb21830d12ab2f835224ffb99f16
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-14 09:42:08 -0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+//Chamando a Bibilioteca
+package/*Biblioteca */ Conhecimento/*Nome */;
+
+public class/*Classe publica */ Test /*Nome da classe */{
+	public static void /*Classe vazia */main(String [] args/*Para escrever */) {
+		System.out.println("Repetindo: o primeiro programa em Java");
+		/*Computador pedindo para escrever */
+		
+	}
+}
