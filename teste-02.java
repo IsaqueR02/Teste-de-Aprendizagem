cb95541dbffd3017c16985065ce81576c48b3a55
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-14 11:08:22 -0300

    Create teste-02.java

---
+++ teste-02.java
@@ -0,0 +1,11 @@
+package Conhecimento;
+
+public class Test{
+	public static void main(String [] args) {
+		//criando variavel
+		/*Declaração da variavel*/int 
+		/*Nome da variavel*/idade = 19/*Valor da variavel*/;
+		System.out.println("Minha idade é: "+ idade/* chamando a variavel*/); 
+		
+	}
+}
