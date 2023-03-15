31b44921b608a28d410492682613bea4379b2bd8
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-15 10:35:57 -0300

    Update teste-02.java

--- teste-02.java
+++ teste-02.java
@@ -6,6 +6,10 @@ public class Test{
 		/*Declaração da variavel*/int 
 		/*Nome da variavel*/idade = 19/*Valor da variavel*/;
 		System.out.println("Minha idade é: "+ idade/* chamando a variavel*/); 
-		
+		idade = 26;
+		//Quando cria um novo valor ele passa a ser esse, pois segue a sequencia
+		System.out.println("Minha idade é: "+ idade);
+		idade = 28;
+		System.out.println("Minha idade é: "+ idade);
 	}
 }
