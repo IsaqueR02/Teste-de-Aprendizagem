23fce28f390dbee48ca18a1d79b5e9a5279b3e40
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-15 11:15:40 -0300

    Create test-04(02).java

---
+++ Test-04/test-04(02).java
@@ -0,0 +1,15 @@
+
+package Conhecimento;
+
+public class Test {
+	public static void main(String [] args) {
+		//Arrays
+		//Aray Multidmensional
+		String[]/*Armazenagem de dados*/ nome = new String[5]/*Definir um limite*/;
+		nome[0]/*Valor armazendado*/ = "Gil";
+		nome[1] = "Jão";
+	String [][] /*Array multidimensional*/ var2 = new String[5][5];
+	var2[0][0] = "Felipe";
+	System.out.println(var2[0][0]);
+	}
+}
