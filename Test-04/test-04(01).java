4ba14031bc7476bb585f315cdc4785c0418a1ff8
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-15 11:15:09 -0300

    Update test-04(01).java

--- Test-04/test-04(01).java
+++ Test-04/test-04(01).java
@@ -3,7 +3,7 @@ package Conhecimento;
 public class Test {
 	public static void main(String [] args) {
 		//Arrays
-		
+		// Array normal
 		String[]/*Armazenagem de dados*/ nome = new String[5]/*Definir um limite*/;
 		nome[0]/*Valor armazendado*/ = "Gil";
 		nome[1] = "Jão";
