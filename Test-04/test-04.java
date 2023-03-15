4e9657ca5c749c55cbf438f12507138c70d4b443
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-15 11:10:42 -0300

    Create test-04.java

---
+++ Test-04/test-04.java
@@ -0,0 +1,12 @@
+package Conhecimento;
+
+public class Test {
+	public static void main(String [] args) {
+		//Arrays
+		
+		String[]/*Armazenagem de dados*/ nome = new String[5]/*Definir um limite*/;
+		nome[0]/*Valor armazendado*/ = "Gil";
+		nome[1] = "Jão";
+		System.out.println("O nome é "+ nome[0])/*Indentificação de dados*/;
+	}
+}
