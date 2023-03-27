53737fc7187aaf72f8789f440f5046a14e03babb
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-27 16:26:34 -0300

    Create test-7.java

---
+++ test-7.java
@@ -0,0 +1,17 @@
+public class Test {
+	public static void main(String [] args) {
+// logica avançada
+		String nome_jogador = "Guilherme";
+		int skill = 100;
+		/*
+		f(nome_jogador == "Guilherme" && skill == 100) {
+			System.out.println("verdade");
+		}else {
+			System.out.println("entramos no else");
+		}
+		*/
+		if(nome_jogador == "Guilherme" || nome_jogador == "Joao" && skill == 100);{
+			System.out.println("Comece o jogo!");
+		}
+	}
+}
