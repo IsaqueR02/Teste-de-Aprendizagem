5e17cb74a81b3e9c7fcdfdba3666f51f0b4ff381
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-03-29 08:33:43 -0300

    Create test-07.java

---
+++ test-07.java
@@ -0,0 +1,22 @@
+public class Test {
+	public static void main(String [] args) {
+		//Looping
+		
+		int contador = 1;
+		
+		/*while(contador <= 10){
+			System.out.println("Olá, agora o contador está em: " +contador);
+			contador++;
+		}*/
+		
+		/*
+		for(int i = 0; i<=10;i++){
+			System.out.println("Olá, agora o contador está em: " +i);
+		}*/
+		contador = 0;
+		do {
+			System.out.println(contador);
+			contador++;
+		}while(contador < 3);
+		}
+}
